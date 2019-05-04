5cfd13abe17d1f579c931aa3f4e5880f24a51057
xyzxyz1337 <gawi@1thecity.biz>
2019-05-05 00:41:11 +0300

    Change models.py
    Change primary-key id -> name
    Add method __str__
    Rewrite method Chat.all_messages

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -9,7 +9,7 @@ class User(models.BaseModel):
         db_table = "users"
         order_by = ("date_register")
 
-    username = peewee.CharField(max_length=20, unique=True)
+    username = peewee.CharField(max_length=20, unique=True, primary_key=True)
     # password = sha-1(salt + password)
     password = peewee.CharField(max_length=192)
     online = peewee.BooleanField(default=True)
@@ -17,3 +17,6 @@ class User(models.BaseModel):
     # datetime.utcfromtimestamp(a).strftime('%Y-%m-%d %H:%M:%S')
     date_register = peewee.TimestampField()
     date_last_online = peewee.TimestampField()
+
+    def __str__(self):
+        return self.username
--- backend/chat/models.py
+++ backend/chat/models.py
@@ -12,17 +12,20 @@ class Chat(BaseModel):
     # Maybe use `unique` is not true way, becouse
     # if two chats it is name user.
     # or user name = owner+name in db
-    name = peewee.CharField(max_length=32, unique=True)
+    name = peewee.CharField(max_length=32, unique=True, primary_key=True)
     owner = peewee.ForeignKeyField(User)
+    password = peewee.CharField(max_length=192, null=True)
+    closed = peewee.BooleanField(default=False)
     date_last_send = peewee.TimestampField()
+
+    def __str__(self):
+        return self.name
 
     @classmethod
     async def all_chats(cls, manager):
-        return await manager.execute(cls.select())
-
-    async def n_messages(self, manager, num):
-        """ Change on return n - messages """
-        return await manager.prefetch(self.messages, User.select())
+        chats = await manager.execute(cls.select())
+        return {"Type": "chat", "Chats": [{"Chat":chat.name,
+                                     "Closed":chat.closed} for chat in chats]}
 
 
 class Message(BaseModel):
