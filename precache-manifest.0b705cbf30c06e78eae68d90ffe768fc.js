776aa20c54f216b7fe6f9cee36f6c2e73bf6fe0d
Cyber_pineapple <cyber_pineapple@mail.ru>
2019-07-13 21:55:55 +0300

    Updates

---
+++ precache-manifest.0b705cbf30c06e78eae68d90ffe768fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b29b843a73226bce00e692f1dc850da",
+    "url": "/messenger_project/static/media/new_message.8b29b843.mp3"
+  },
+  {
+    "revision": "bd29e7e9e5d2dc6928ae2fef2a43771d",
+    "url": "/messenger_project/static/media/free_hexagon_pattern_02_by_black_light_studio-d65g32i.bd29e7e9.png"
+  },
+  {
+    "revision": "3380bfed6d949d1236b2d43066ab6018",
+    "url": "/messenger_project/static/media/anonymous_mask1600.3380bfed.png"
+  },
+  {
+    "revision": "d67f5490d63829394129",
+    "url": "/messenger_project/static/js/runtime~main.9519198f.js"
+  },
+  {
+    "revision": "b9b1f6d3b0829f741891",
+    "url": "/messenger_project/static/js/main.782ad76d.chunk.js"
+  },
+  {
+    "revision": "1e21f5c77cb456f41387",
+    "url": "/messenger_project/static/js/2.bcb2c228.chunk.js"
+  },
+  {
+    "revision": "b9b1f6d3b0829f741891",
+    "url": "/messenger_project/static/css/main.e480dce6.chunk.css"
+  },
+  {
+    "revision": "b99ab4f9d512ef544ca6bbc3eae2a321",
+    "url": "/messenger_project/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.083909e40961efc7adec28cf6961536d.js"
+  "/messenger_project/precache-manifest.0b705cbf30c06e78eae68d90ffe768fc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/messenger_project/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
